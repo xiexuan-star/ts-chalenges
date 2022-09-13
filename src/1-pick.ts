41459ca997b6fabd05d1107429770a557569543d
xiexuan-star <xiexuan127@foxmail.com>
2022-09-13 14:50:36 +0800

    feat: second readonly

--- src/1-pick.ts
+++ src/1-pick.ts
@@ -1,24 +1,27 @@
-// 实现 TS 内置的 Pick<T, K>，但不可以使用它。
+{
+  // 实现 TS 内置的 Pick<T, K>，但不可以使用它。
 //
 // 从类型 T 中选择出属性 K，构造成一个新的类型。
 //
 // 例如：
 
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
-}
+  interface Todo {
+    title: string;
+    description: string;
+    completed: boolean;
+  }
 
-type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+  type TodoPreview = MyPick<Todo, 'title' | 'completed'>
 
-const todo: TodoPreview = {
-  title: 'Clean room',
-  completed: false,
-};
+  const todo: TodoPreview = {
+    title: 'Clean room',
+    completed: false,
+  };
 
 // ------------------------------------ answer ---------------------------------
 
-type MyPick<O, P extends keyof O> = {
-  [K in P]: O[K]
+  type MyPick<O, P extends keyof O> = {
+    [K in P]: O[K]
+  }
+
 }
