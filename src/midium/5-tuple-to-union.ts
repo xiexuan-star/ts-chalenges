1f5d2dc14c87f79a4704b1c3b50b2c5afd6387db
xiexuan-star <xiexuan127@foxmail.com>
2022-09-14 11:44:19 +0800

    feat: tuple-to-union

---
+++ src/midium/5-tuple-to-union.ts
@@ -0,0 +1,11 @@
+{
+  // Implement a generic TupleToUnion<T> which covers the values of a tuple to its values union.
+
+  // For example
+
+  type Arr = ['1', '2', '3']
+
+  type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
+
+  type TupleToUnion<T extends readonly unknown[]> = T[number]
+}
