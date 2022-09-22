6373d2422cabe945ad7bafa1f1f3851e2d99c911
xiexuan-star <xiexuan127@foxmail.com>
2022-09-22 09:07:42 +0800

    feat: append-argument

---
+++ src/midium/16-append-argument.ts
@@ -0,0 +1,13 @@
+{
+  // For given function type Fn, and any type A (any in this context means we don't restrict the type, and I don't have in mind any type 😉) create a generic type which will take Fn as the first argument, A as the second, and will produce function type G which will be the same as Fn but with appended argument A as a last one.
+
+  // For example,
+
+  type Fn = (a: number, b: string) => number
+
+  type Result = AppendArgument<Fn, boolean>
+
+// expected be (a: number, b: string, x: boolean) => number
+
+  type AppendArgument<F extends (...args: any[]) => any, ARG> = F extends (...args: infer A) => infer R ? (...args: [...A, ARG]) => R : never
+}
