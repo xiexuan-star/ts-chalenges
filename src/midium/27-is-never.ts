4a4f703454e00839cfadb94110fea0f0777e9087
xiexuan-star <xiexuan127@foxmail.com>
2022-10-19 09:49:26 +0800

    feat: is-never

---
+++ src/midium/27-is-never.ts
@@ -0,0 +1,14 @@
+{
+  // Implement a type IsNever, which takes input type T. If the type of resolves to never, return true, otherwise false.
+
+  // For example:
+
+  type A = IsNever<never>  // expected to be true
+  type B = IsNever<undefined> // expected to be false
+  type C = IsNever<null> // expected to be false
+  type D = IsNever<[]> // expected to be false
+  type E = IsNever<number> // expected to be false
+
+  type IsNever<T> = Record<string, T> extends Record<string, never> ? true : false
+  type IsNever2<T> = [T] extends [never] ? true : false
+}
