12f891a9fcf23565bf65a1aa9865ffc8675d91de
xiexuan-star <xiexuan127@foxmail.com>
2022-11-30 10:55:42 +0800

    feat: object-from-entries

---
+++ src/hard/29-object-from-entries.ts
@@ -0,0 +1,59 @@
+/*
+  2949 - ObjectFromEntries
+  -------
+  by jiangshan (@jiangshanmeta) #hard #object
+
+  ### Question
+
+  Implement the type version of ```Object.fromEntries```
+
+  For example:
+
+  ```typescript
+  interface Model {
+    name: string;
+    age: number;
+    locations: string[] | null;
+  }
+
+  type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null];
+
+  type result = ObjectFromEntries<ModelEntries> // expected to be Model
+  ```
+
+  > View on GitHub: https://tsch.js.org/2949
+*/
+
+
+/* _____________ Your Code Here _____________ */
+type ObjectFromEntries<T extends [keyof any, unknown]> = {
+  [K in T as K[0]]: K[1]
+}
+
+declare const foo: {
+  [K in ['a'] | ['b'] | ['c'] as K[0]]: K[0]
+} extends Record<any, infer V> ? V : never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null]
+
+type cases = [
+  Expect<Equal<ObjectFromEntries<ModelEntries>, Model>>,
+]
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2949/answer
+  > View solutions: https://tsch.js.org/2949/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
